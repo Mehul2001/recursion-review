c8acce900af1869d5e44471a601c1593ca2b418e
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-01-02 23:02:58 -0800

    Completed the basic requirements of the program.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,126 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+//if number || string || null || boolean 
+//if array loop through all the elements of array and convert into string
+// if object loop through all the elements of object values and convert into string
+  var resultArr = ""; 
+  if(typeof(obj) === 'number' || obj === null || obj === true || obj === false){
+    return String(obj);
+  }
+  if(typeof(obj) === 'string'){
+    return '"' + obj  + '"';
+  }
+  if(Array.isArray(obj)) {   
+    if(obj.length === 0){
+      return '[]';
+    } else{
+      var ans = '[';
+      for(var i = 0; i < obj.length; i++){
+        ans+= stringifyJSON(obj[i]) + ',';
+      }
+      return ans.slice(0, ans.length-1) + ']';
+    }
+  }
+  if(typeof obj === 'object'){
+    var keys = Object.keys(obj);
+    if(keys.length === 0){
+      return '{}';
+    } else{
+      var objAns = "";
+      for(var j = 0; j < keys.length; j++){
+        if(typeof obj[keys[j]] === 'function'){
+          return "{}";
+        }
+        if(j === keys.length-1){
+          objAns += stringifyJSON(keys[j]) + ":" + stringifyJSON(obj[keys[j]]);
+        } else {
+          objAns += stringifyJSON(keys[j]) + ":" + stringifyJSON(obj[keys[j]]) + ",";
+        }    
+      }
+      return "{" + objAns + "}";
+    }
+  }
+};var stringifyJSON = function(obj) {
+//if number || string || null || boolean 
+//if array loop through all the elements of array and convert into string
+// if object loop through all the elements of object values and convert into string
+  var resultArr = ""; 
+  if(typeof(obj) === 'number' || obj === null || obj === true || obj === false){
+    return String(obj);
+  }
+  if(typeof(obj) === 'string'){
+    return '"' + obj  + '"';
+  }
+  if(Array.isArray(obj)) {   
+    if(obj.length === 0){
+      return '[]';
+    } else{
+      var ans = '[';
+      for(var i = 0; i < obj.length; i++){
+        ans+= stringifyJSON(obj[i]) + ',';
+      }
+      return ans.slice(0, ans.length-1) + ']';
+    }
+  }
+  if(typeof obj === 'object'){
+    var keys = Object.keys(obj);
+    if(keys.length === 0){
+      return '{}';
+    } else{
+      var objAns = "";
+      for(var j = 0; j < keys.length; j++){
+        if(typeof obj[keys[j]] === 'function'){
+          return "{}";
+        }
+        if(j === keys.length-1){
+          objAns += stringifyJSON(keys[j]) + ":" + stringifyJSON(obj[keys[j]]);
+        } else {
+          objAns += stringifyJSON(keys[j]) + ":" + stringifyJSON(obj[keys[j]]) + ",";
+        }    
+      }
+      return "{" + objAns + "}";
+    }
+  }
+};var stringifyJSON = function(obj) {
+//if number || string || null || boolean 
+//if array loop through all the elements of array and convert into string
+// if object loop through all the elements of object values and convert into string
+  var resultArr = ""; 
+  if(typeof(obj) === 'number' || obj === null || obj === true || obj === false){
+    return String(obj);
+  }
+  if(typeof(obj) === 'string'){
+    return '"' + obj  + '"';
+  }
+  if(Array.isArray(obj)) {   
+    if(obj.length === 0){
+      return '[]';
+    } else{
+      var ans = '[';
+      for(var i = 0; i < obj.length; i++){
+        ans+= stringifyJSON(obj[i]) + ',';
+      }
+      return ans.slice(0, ans.length-1) + ']';
+    }
+  }
+  if(typeof obj === 'object'){
+    var keys = Object.keys(obj);
+    if(keys.length === 0){
+      return '{}';
+    } else{
+      var objAns = "";
+      for(var j = 0; j < keys.length; j++){
+        if(typeof obj[keys[j]] === 'function'){
+          return "{}";
+        }
+        if(j === keys.length-1){
+          objAns += stringifyJSON(keys[j]) + ":" + stringifyJSON(obj[keys[j]]);
+        } else {
+          objAns += stringifyJSON(keys[j]) + ":" + stringifyJSON(obj[keys[j]]) + ",";
+        }    
+      }
+      return "{" + objAns + "}";
+    }
+  }
 };
