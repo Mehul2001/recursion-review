e7155ed4112159d5f42deff4a932c78d25b03c8c
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-01-02 23:27:29 -0800

    Completed basic requirements.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  if(!window.document.body){
+  	return null;
+  };
+  var result = [];
+  function wrap(currentNode){
+      if (currentNode.classList.contains(className)){    result.push(currentNode);
+    };
+    if(currentNode.children.length === 0){
+    	return result;
+    } else {
+    	for(var i = 0; i < currentNode.children.length;i++){
+    	  var curVal = currentNode.children[i];
+        wrap(curVal);
+    	};
+    };
+  }
+  wrap(window.document.body)
+ return result;
 };
